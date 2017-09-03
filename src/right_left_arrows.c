0d734287699cc1fd3c2efd4b5957bdd3a7050dba
Oleksandr Podolian <opodolia@e3r2p4.unit.ua>
2017-09-03 19:29:32 +0300

    multiple history movement done

--- src/right_left_arrows.c
+++ src/right_left_arrows.c
@@ -6,7 +6,7 @@
 /*   By: opodolia <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/08 20:01:30 by opodolia          #+#    #+#             */
-/*   Updated: 2017/08/22 19:17:59 by opodolia         ###   ########.fr       */
+/*   Updated: 2017/09/03 19:27:55 by opodolia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,9 @@ void		move_right(void)
 
 void		move_left(void)
 {
-	int		tmp;
 	int		flag;
+	int		line_indx;
+	int		byte_indx;
 
 	if (g_info->position % g_info->win_size != 0 &&
 		g_info->line[g_info->line_index - 1] != '\n')
@@ -37,32 +38,47 @@ void		move_left(void)
 		g_info->line[g_info->line_index - 1] == '\n')
 	{
 		ft_putstr("\033M");
-		tmp = 0;
+		line_indx = 0;
 		if (g_info->line[g_info->line_index - 1] == '\n')
 		{
 			flag = 0;
-			tmp = g_info->line_index - 1;
-			while (g_info->line[tmp--])
-				if (g_info->line[tmp] == '\n')
+			line_indx = g_info->line_index - 2;
+			byte_indx = g_info->bytes_index - 1;
+			while (g_info->line[line_indx])
+			{
+				if (g_info->line[line_indx - 1] == '\n')
 					flag = 1;
+				line_indx -= g_info->bytes_str[byte_indx] - '0';
+				byte_indx--;
+			}
 			if (!flag)
 			{
-				tmp = g_info->line_index - 1 + g_info->prompt_len;
-				while (tmp-- > 0)
+				line_indx = g_info->line_index - 1;
+				byte_indx = g_info->bytes_index - 2;
+				while (line_indx > 0)
+				{
+					tputs(tgetstr("nd", 0), 1, &ft_put_my_char);
+					line_indx -= g_info->bytes_str[byte_indx] - '0';
+					byte_indx--;
+				}
+				line_indx = 0;
+				while (line_indx++ < g_info->prompt_len)
 					tputs(tgetstr("nd", 0), 1, &ft_put_my_char);
 			}
 			else
 			{
-				tmp = g_info->line_index - 2;
-				while (g_info->line[tmp] && g_info->line[tmp] != '\n')
+				line_indx = g_info->line_index - 2;
+				byte_indx = g_info->bytes_index - 2;
+				while (g_info->line[line_indx] && g_info->line[line_indx] != '\n')
 				{
 					tputs(tgetstr("nd", 0), 1, &ft_put_my_char);
-					tmp--;
+					line_indx -= g_info->bytes_str[byte_indx] - '0';
+					byte_indx--;
 				}
 			}
 		}
 		else
-			while (tmp++ < g_info->win_size - 1)
+			while (line_indx++ < g_info->win_size - 1)
 				tputs(tgetstr("nd", 0), 1, &ft_put_my_char);
 	}
 	g_info->line_index -= g_info->bytes_str[g_info->bytes_index - 1] - '0';
